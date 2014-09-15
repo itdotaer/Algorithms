07f3cd3a30ffc8c386edec7fa6f9705924974e2a
SoftwareDreamer <hujiangtao1235@gmail.com>
2014-09-15 13:48:38 +0800

    Add enum and struct

--- AssignCandy/Program.cs
+++ AssignCandy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 
 namespace AssignCandy
 {
---
+++ EnumAndStruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EnumAndStruct
+{
+    public enum Week
+    {
+        Sun = 1,Mon,Tue,Wed,Thu,Fri,Sat
+    }
+
+    public struct DataStruct
+    {
+        public string name;
+        public int age;
+        public string des;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rs = Week.Mon;
+            Console.WriteLine((int)Week.Mon);
+
+            var dataStruct = new DataStruct {name = "jodif", age = 22, des = "djfiosdjf"};
+
+
+            Console.Read();
+        }
+    }
+}
